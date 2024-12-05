910a9b8ced8ada036e3e95c238b9ddbe512f4dd2
liamwyj <120946947+liamwyj@users.noreply.github.com>
2024-12-05 12:52:59 +0100

    Delete 7311-01-P2B directory

--- 7311-01-P2B/src/plugins/axios.js
+++
@@ -1,8 +0,0 @@
-// src/plugins/axios.js
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9000', // URL servidor Flask
-});
-
-export default axiosInstance;
