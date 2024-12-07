56dbcefb3f722ebdb5353fa5ab0276224fcd7864
liamwyj <120946947+liamwyj@users.noreply.github.com>
2024-12-07 13:23:51 +0100

    axios.js

--- 7311-01-P2B/frontend/src/plugins/axios.js
+++ 7311-01-P2B/frontend/src/plugins/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9000', // URL servidor Flask
+  baseURL: 'https://compdistr.onrender.com', // URL servidor Flask
 });
 
 export default axiosInstance;
